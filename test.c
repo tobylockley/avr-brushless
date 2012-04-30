cc51a9d07981c2afff60e7870fed3b8ebd440b2c
Toby Lockley <tobylockley@gmail.com>
2012-05-01 00:05:33 +1000

    second commit

---
+++ test.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  testing git commit
+ *
+ *        Version:  1.0
+ *        Created:  30/04/12 23:56:03
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
+ *        Company:  FH Südwestfalen, Iserlohn
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+void main()
+{
+    printf("Hello World!\n");
+}
